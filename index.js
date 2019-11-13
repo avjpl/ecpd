dc35beed5f5556de985b6d5e163dbbfe8c0d86b7
Adrian Lawrence <avjpldeveloper@aol.com>
2019-11-13 18:48:03 +0000

    write data to file

--- index.js
+++ index.js
@@ -4,35 +4,29 @@ const { parser } = require('stream-json');
 const { streamValues } = require('stream-json/streamers/StreamValues');
 
 const objs = [];
+const writer = fs.createWriteStream('./data-trimed.json');
 
 fs.createReadStream('./data.json')
-    .pipe(parser())
-    .pipe(streamValues())
-    .pipe(through2({ objectMode: true }, function (data, enc, callback) {
-        data.value.ChargeDevice.map((device) => {
-            // console.log(device);
-            // console.log('\n');
-            // console.log('\n');
-            this.push({
-                ChargeDeviceName: device.ChargeDeviceName,
-                ChargeDeviceLocation: device.ChargeDeviceLocation,
-                ChargeDeviceManufacturer: device.ChargeDeviceManufacturer,
-                ChargeDeviceModel: device.ChargeDeviceModel,
-                Attribution: device.Attribution,
-                Connector: device.Connector,
-                DeviceOwner: device.DeviceOwner,
-            });
-        });
-
-        callback();
-    }))
-    .on('data', (data) => {
-        // console.log(data);
-        // console.log('\n');
-        objs.push(data);
-    })
-    .on('end', () => {
-        console.log(JSON.stringify(objs))
+  .pipe(parser())
+  .pipe(streamValues())
+  .pipe(through2({ objectMode: true }, function (data, enc, callback) {
+    data.value.ChargeDevice.map((device) => {
+      this.push({
+        ChargeDeviceName: device.ChargeDeviceName,
+        ChargeDeviceLocation: device.ChargeDeviceLocation,
+        ChargeDeviceManufacturer: device.ChargeDeviceManufacturer,
+        ChargeDeviceModel: device.ChargeDeviceModel,
+        Attribution: device.Attribution,
+        Connector: device.Connector,
+        DeviceOwner: device.DeviceOwner,
+      });
     });
 
-// .pipe(fs.createWriteStream('./data-trimed.json'))
+    callback();
+  }))
+  .on('data', data => {
+    objs.push(data);
+  })
+  .on('end', () => {
+    writer.write(JSON.stringify({data: objs}));
+  });
